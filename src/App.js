3c271ec1fdca50b0609fe639a43bb25d40b09e4a
Justin Cook <justin.cook.dev@gmail.com>
2019-06-06 19:14:52 -0700

    Initial project scaffolding complete.

---
+++ src/App.js
@@ -0,0 +1,5 @@
+import React, { Component } from 'react';
+
+const App = () => <div>Hello</div>;
+
+export default App;
---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+const path = require('path');
+const webpack = require('webpack');
+
+module.exports = {
+  mode: 'development',
+  entry: ['@babel/polyfill', './src/index.js'],
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+    publicPath: '/',
+  },
+  devServer: {
+    contentBase: path.join(__dirname, 'dist'),
+    compress: true,
+    port: 8080,
+    hot: true,
+    historyApiFallback: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: 'babel-loader',
+      },
+    ],
+  },
+  plugins: [new webpack.HotModuleReplacementPlugin()],
+};
